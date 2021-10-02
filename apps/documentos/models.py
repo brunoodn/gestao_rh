fa158a60fae03ded871b49e46e4b35bffff7c78b
Bruno Oliveira <bruno.odn@gmail.com>
2021-10-01 22:45:59 -0300

    criando o relacionamento entre os models

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionario, models.PROTECT)
 
     def __str__(self):
         return self.descricao
