8353d3b9fe91bb2f09fa1581b8cf6d5c84a8d376
Bruno Oliveira <bruno.odn@gmail.com>
2021-10-07 22:39:14 -0300

    Terminando o crud hora extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HoraExtraList
+from .views import HoraExtraList, HoraExtraEdit, HoraExtraDelete, HoraExtraNovo
 
 urlpatterns = [
-    path('novo/',HoraExtraList.as_view() , name='list_hora_extra')
+    path('list/', HoraExtraList.as_view() , name='list_hora_extra'),
+    path('novo/', HoraExtraNovo.as_view() , name='create_hora_extra'),
+    path('editar/<int:pk>', HoraExtraEdit.as_view() , name='update_hora_extra'),
+    path('delete/<int:pk>', HoraExtraDelete.as_view() , name='delete_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import RegistroHoraExtraForm
 from .models import RegistroHoraExtra
-# Create your views here.
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 
 class HoraExtraList(ListView):
@@ -10,3 +10,27 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+class HoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+    #fields = ['motivo', 'funcionario', 'horas']
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
